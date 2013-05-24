9e1ee5921d496b6df00b7490f7529761b8c1a8a4
dave <davefowler@gmail.com>
2013-05-24 01:28:58 -0700

    added pin.js file for megan

---
+++ content/files/pin.js
@@ -0,0 +1,11 @@
+    
+var youtubelink = jQuery('object param[name="movie"]').val();
+var youtubecode = youtubelink.match(/\/v\/(.*)\?/)[1];
+var thumbnail = 'http://img.youtube.com/vi/' + youtubecode + '/0.jpg';
+var button = '<a href="//pinterest.com/pin/create/button/?url='+escape(window.location.href)+'&media=' + escape(thumbnail) + '&description=' + escape(document.title) + '" data-pin-do="buttonPin" data-pin-config="above"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>'
+
+jQuery(document).ready(function() {
+  jQuery('object').append(
+    '<p>'+ button + '</p>'
+  )
+});
