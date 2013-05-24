45827912424c706d57f5335a45a6fdeaf92004fb
dave <davefowler@gmail.com>
2013-05-24 01:43:47 -0700

    fixes to pin.js

--- content/media/js/pin.js
+++ content/media/js/pin.js
@@ -1,11 +1,12 @@
 /* this is for Megan's almostathirdgrader site */
 
-var youtubelink = jQuery('object param[name="movie"]').val();
-var youtubecode = youtubelink.match(/\/v\/(.*)\?/)[1];
-var thumbnail = 'http://img.youtube.com/vi/' + youtubecode + '/0.jpg';
-var button = '<a href="//pinterest.com/pin/create/button/?url='+escape(window.location.href)+'&media=' + escape(thumbnail) + '&description=' + escape(document.title) + '" data-pin-do="buttonPin" data-pin-config="above"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>'
 
 jQuery(document).ready(function() {
+  var youtubelink = jQuery('object param[name="movie"]').val();
+  var youtubecode = youtubelink.match(/\/v\/(.*)\?/)[1];
+  var thumbnail = 'http://img.youtube.com/vi/' + youtubecode + '/0.jpg';
+  var button = '<a href="//pinterest.com/pin/create/button/?url='+escape(window.location.href)+'&media=' + escape(thumbnail) + '&description=' + escape(document.title) + '" data-pin-do="buttonPin" data-pin-config="above"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>'
+
   jQuery('object').append(
     '<p>'+ button + '</p>'
   )
